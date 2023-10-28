46ef3aec163f7a1f5ed81e3e189294bb1b22bcc2
MustafaAH10 <mustafahussain793@gmail.com>
2023-10-28 19:21:05 +0800

    Try and fix image issues

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,19 +1,8 @@
 // Blog.js
 import React from "react";
 import blogPosts from "../blogData";
-import CodeBlock from "../CodeBlock";
 import { useParams } from "react-router-dom";
 import "../styles/Blog.css"; // Import the Blog.css styles
-
-function NewlineText({ text }) {
-	const newText = text.split("\n").map((str, index, array) => (
-		<React.Fragment key={index}>
-			<p className="text-content">{str}</p>
-		</React.Fragment>
-	));
-
-	return <>{newText}</>;
-}
 
 function Blog() {
 	const { postId } = useParams();
@@ -22,23 +11,6 @@ function Blog() {
 	if (!post) return <p>Post not found!</p>;
 
 	return (
-		// <div className="blog-post-container">
-		// 	<h1>{post.title}</h1>
-		// 	{post.content.map((item, i) => {
-		// 		switch (item.type) {
-		// 			case "text":
-		// 				return <NewlineText key={i} text={item.value} />;
-		// 			case "code":
-		// 				return <CodeBlock key={i} code={item.value} />;
-		// 			case "image":
-		// 				return (
-		// 					<img key={i} src={item.value} alt="" className="blog-image" />
-		// 				);
-		// 			default:
-		// 				return null;
-		// 		}
-		// 	})}
-		// </div>
 		<div className="blog-post-container">
 			<h1>{post.title}</h1>
 			{post.content}
